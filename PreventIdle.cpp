b24fabe58543f2d9f67cd5f9ee63faddd33280e7
Chris Lomont <chris@lomont.org>
2021-05-13 08:51:21 -0400

    Initial Check in

---
+++ PreventIdle.cpp
@@ -0,0 +1,20 @@
+// Chris Lomont, 2021, www.lomont.org
+// prevent windows idle to stop screen savers, security logouts, etc.
+// MIT Licensed - Copyright (c) 2021 Chris Lomont
+
+#include <windows.h>
+#include <cstdio>
+
+int main()
+{
+	printf("Prevent windows locking (via F24 key presses). Press q to quit.  ");
+	auto pass = 0;
+	while (!GetKeyState(0x51)) // check for Q
+	{
+		printf("%c\b","/-\\|"[(pass++)&3]);
+		keybd_event(VK_F24,0,0,0);
+		Sleep(100);
+	}
+	printf("\nDone.\n");
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Prevent Idle
+
+Use this to stop auto-logout on Windows 10.
+
+Simulates pressing the function key F24, which is not commonly used, or even available.
+
+Short C++ windows code, compiled using visual studio 2019.
+
+Compile yourself, or simply use the executable.
+
